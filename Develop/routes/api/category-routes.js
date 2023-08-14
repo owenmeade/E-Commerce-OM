187caa56bc976dba71b1111c84d8486119242905
Owen M <drawowen77@gmai.com>
2023-08-14 15:11:22 -0700

    fix server js and models, add env file and start on routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,6 +5,18 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      },
+    ],
+  }).then((categoryData) => res.json(categoryData)).catch((err ) => {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // be sure to include its associated Products
 });
 
